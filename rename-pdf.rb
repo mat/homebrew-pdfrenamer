8569f8c21f6a33e0fe4ef882003e523998616013
Matthias Luedtke <email@matthias-luedtke.de>
2019-12-09 22:05:09 +0100

    Update formula

--- rename-pdf.rb
+++ rename-pdf.rb
@@ -2,12 +2,12 @@ require "formula"
 
 class RenamePdf < Formula
   homepage "https://github.com/mat/pdfrenamer"
-  url "https://github.com/mat/homebrew-rename-pdf/releases/download/0.1/pdfrenamer.jar"
-  sha256 "b91a5c1522ced7de9a3b0f243c5cbbe064cc04eb792b49eafe185fc363ae14e0"
+  url "https://github.com/mat/homebrew-rename-pdf/releases/download/0.1/rename-pdf.zip"
+  sha256 "1875b1273768e4b30f819261b26e8cd677b1d085adb63a6356d3e7f7afb3fc16"
 
   def install
       inreplace "brew/rename-pdf", "##PREFIX##", "#{prefix}"
       prefix.install "pdfrenamer.jar"
-      # bin.install "brew/rename-pdf"
+      bin.install "brew/rename-pdf"
   end
 end
