e4cc996f68aead98a657277a5d512b30c384bac8
Matthias Luedtke <email@matthias-luedtke.de>
2019-12-09 21:54:56 +0100

    first commit

---
+++ rename-pdf.rb
@@ -0,0 +1,13 @@
+require "formula"
+
+class RenamePdf < Formula
+  homepage "https://github.com/mat/pdfrenamer"
+  url "https://.....pdfrenamer.jar"
+  sha256 "b91a5c1522ced7de9a3b0f243c5cbbe064cc04eb792b49eafe185fc363ae14e0"
+
+  def install
+      inreplace "brew/rename-pdf", "##PREFIX##", "#{prefix}"
+      prefix.install "pdfrenamer.jar"
+      # bin.install "brew/rename-pdf"
+  end
+end
