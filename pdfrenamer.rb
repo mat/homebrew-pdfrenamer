23b5d67654db448da97d388fb9419c29482aca0d
Matthias Luedtke <email@matthias-luedtke.de>
2019-12-09 22:15:31 +0100

    Rename eveything to pdfrenamer (w/o the dash)

---
+++ pdfrenamer.rb
@@ -0,0 +1,13 @@
+require "formula"
+
+class RenamePdf < Formula
+  homepage "https://github.com/mat/pdfrenamer"
+  url "https://github.com/mat/homebrew-pdfrenamer/releases/download/0.1/pdfrenamer.zip"
+  sha256 "54495358a4de3d5ca7fcb5d35adcbe3ba5ffe12f7b0edba2c985bd5c326a8c9f"
+
+  def install
+      inreplace "brew/pdfrenamerx", "##PREFIX##", "#{prefix}"
+      prefix.install "pdfrenamer.jar"
+      bin.install "brew/pdfrenamerx"
+  end
+end
