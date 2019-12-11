c313fe5f2b7f52b02ec0000827a9128eb861d171
Matthias Luedtke <email@matthias-luedtke.de>
2019-12-11 22:30:01 +0100

    Update sha 🙄

--- pdfrenamer.rb
+++ pdfrenamer.rb
@@ -3,7 +3,7 @@ require "formula"
 class Pdfrenamer < Formula
   homepage "https://github.com/mat/pdfrenamer"
   url "https://github.com/mat/homebrew-pdfrenamer/releases/download/0.1/pdfrenamer.zip"
-  sha256 "54495358a4de3d5ca7fcb5d35adcbe3ba5ffe12f7b0edba2c985bd5c326a8c9f"
+  sha256 "a8b953fd996c7090830ab3d0b1d202f17eea4e02a3ef6a2ce7f162def39351db"
 
   def install
       inreplace "brew/pdfrenamer", "##PREFIX##", "#{prefix}"
