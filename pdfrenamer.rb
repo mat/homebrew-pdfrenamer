addecc3a4cd69a30cd8371b29130f962851670bb
Matthias Luedtke <email@matthias-luedtke.de>
2019-12-11 22:42:40 +0100

    depends_on :java 8

--- pdfrenamer.rb
+++ pdfrenamer.rb
@@ -5,6 +5,8 @@ class Pdfrenamer < Formula
   url "https://github.com/mat/homebrew-pdfrenamer/releases/download/0.1/pdfrenamer.zip"
   sha256 "a8b953fd996c7090830ab3d0b1d202f17eea4e02a3ef6a2ce7f162def39351db"
 
+  depends_on :java => "1.8+"
+
   def install
       inreplace "brew/pdfrenamer", "##PREFIX##", "#{prefix}"
       prefix.install "pdfrenamer.jar"
