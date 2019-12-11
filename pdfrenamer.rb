1ad5c91d5aed6d81ae3f3bab8671dbbb53b3334e
Matthias Luedtke <email@matthias-luedtke.de>
2019-12-11 23:08:47 +0100

    brew audit pdfrenamer.rb --new-formula

--- pdfrenamer.rb
+++ pdfrenamer.rb
@@ -1,6 +1,5 @@
-require "formula"
-
 class Pdfrenamer < Formula
+  desc "Rename PDFs based on content"
   homepage "https://github.com/mat/pdfrenamer"
   url "https://github.com/mat/homebrew-pdfrenamer/releases/download/0.1/pdfrenamer.zip"
   sha256 "a8b953fd996c7090830ab3d0b1d202f17eea4e02a3ef6a2ce7f162def39351db"
@@ -8,8 +7,8 @@ class Pdfrenamer < Formula
   depends_on :java => "1.8+"
 
   def install
-      inreplace "brew/pdfrenamer", "##PREFIX##", "#{prefix}"
-      prefix.install "pdfrenamer.jar"
-      bin.install "brew/pdfrenamer"
+    inreplace "brew/pdfrenamer", "##PREFIX##", prefix.to_s
+    prefix.install "pdfrenamer.jar"
+    bin.install "brew/pdfrenamer"
   end
 end
